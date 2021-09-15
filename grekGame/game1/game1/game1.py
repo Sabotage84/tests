0c2bff3652f7b9c903e4f5f3a9cb507409d59cde
Sabotage84 <sabotage84@mail.ru>
2021-09-15 23:02:38 +0300

    Add grek game wrong var

--- grekGame/game1/game1/game1.py
+++ grekGame/game1/game1/game1.py
@@ -1,2 +1,20 @@
-print (u'\u25AE\u2589')
-print (u'\u25AE\u2589')
+import os
+import sys
+import time
+
+
+print("Hello\r")
+print(b'\x1b[A')
+print("World")
+
+
+
+
+print (u'\u25AE\u2589\r')
+print (u'\u25AE\u2589\r')
+
+
+i=100
+while i>0:
+    print()
+    i-=1
