8f715481619e17537db00d352f8ba456ae30f447
Sabotage84 <sabotage84@mail.ru>
2021-09-22 22:28:59 +0300

    Add new pyGame

---
+++ mygame1/mein.py
@@ -0,0 +1,79 @@
+import pygame as pygame
+import random
+import os
+import sys
+import time
+
+WIDTH = 600  # ширина игрового окна
+HEIGHT = 700  # высота игрового окна
+FPS = 30  # частота кадров в секунду
+
+# Задаем цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+
+class Player(pygame.sprite.Sprite):
+
+    speed = 5
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = ((WIDTH / 2) - 25, 0)
+
+    def updateSpeed(self):
+        self.speed = 0
+
+    def update(self):
+        self.rect.y += self.speed
+        # if self.rect.left > WIDTH:
+        #     self.rect.right = 0
+
+
+# создаем игру и окно
+pygame.init()
+pygame.mixer.init()  # для звука
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+# Цикл игры
+running = True
+while running:
+    # держим цикл на правильной скорости
+    clock.tick(FPS)
+    # Ввод процесса (события)
+    for event in pygame.event.get():
+        # проверить закрытие окна
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                player.updateSpeed()
+    # Обновление
+    all_sprites.update()
+
+    # Отрисовка
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+
+    # После отрисовки всего, переворачиваем экран
+    pygame.display.flip()
+
+    # Визуализация (сборка)
+
+pygame.quit()
+
+
+
+
+
